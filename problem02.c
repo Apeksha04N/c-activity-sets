d8ec5214fa7ed3e9db4dce5da2a54f8bd54aba95
Apeksha04N <apekshan.bsc23@rvu.edu.in>
2024-02-01 10:25:33 +0530

    test

--- problem02.c
+++ problem02.c
@@ -5,7 +5,6 @@ int main()
     int a=1;
     int b=2;
     int sum=a+b;
-    printf("%d \n",sum);
+    printf("%d\n",sum);
     return 0;
 }
-
---
+++ problem06.c
@@ -0,0 +1,10 @@
+//Write a C program to compare three numbers using **pass by reference**
+#include <stdio.h>
+void input(int *a, int *b, int *c);
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
+int main()
+{
+    int a,b,c;
+    
+}
