b32a75832d8e18181a0d66e84479ea2e57951b8b
Apeksha04N <apekshan.bsc23@rvu.edu.in>
2024-03-01 14:04:37 +0530

    test

--- set02/problem06.c
+++ set02/problem06.c
@@ -5,7 +5,7 @@ void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 int main()
 {
-    char a, str, rev_str, reverse_a;
+    char a, str[10], rev_str[10], reverse_a[10];
     input_string(&a);
     str_reverse(&str, &rev_str);
     output(&a, &reverse_a);
@@ -14,13 +14,15 @@ int main()
 void input_string(char *a)
 {
     printf("Enter the string\n");
-    scanf("%s", a);
+    scanf("%s", &a);
 }
 void str_reverse(char *str, char *rev_str)
 {
-    for (int i = 0; i)
+    for(int i=0; i<=10 ; i++)
     {
-        
+    printf("Enter the string\n");
+    scanf("%s", &str[i]);
+    
     }
 }
 void output(char *a, char *reverse_a)
