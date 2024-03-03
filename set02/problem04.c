3e3ed9ab24997e811dc19131145578e8112c3549
Apeksha04N <apekshan.bsc23@rvu.edu.in>
2024-03-03 11:26:05 +0530

    test

--- set02/problem04.c
+++ set02/problem04.c
@@ -31,13 +31,16 @@ void input_array(int n, int a[n])
 }
 int sum_composite_numbers(int n, int a[n])
 {
-    int sum = 0;
-    for(int i=0; i<n; i++)
+    int sum = 0,count;
+    for (int i = 0; i < n; i++)
     {
-      if(n % i == 0)
-      {
-        sum += i;
-      }
+        for(int j=2;j<a[i];i++)
+        {
+            if(a[i]%j==0)
+            {
+                sum+=a[i];
+            }
+        }
     }
     return sum;
 }
--- set02/problem07.c
+++ set02/problem07.c
@@ -1,19 +1,17 @@
-//07. Write a program to find the area of a triangle.
+// 07. Write a program to find the area of a triangle.
 #include <stdio.h>
 #include <math.h>
-typedef struct _triangle {
+typedef struct _triangle
+{
 	float base, altitude, area;
 } Triangle;
 
 Triangle input_triangle()
 {
-
 }
 void find_area(Triangle *t)
 {
-
 }
 void output(Triangle t)
 {
-	
 }
